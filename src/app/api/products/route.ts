eb5dc10c1c4107e1133ea2200b5a18b75e72b162
AnandBabu <anandbabupanackal2003@gmail.com>
2026-04-29 01:18:59 +0100

    database

---
+++ src/app/api/products/route.ts
@@ -0,0 +1,25 @@
+import { prisma } from "../../../lib/prisma";
+
+export async function GET() {
+  const products = await prisma.product.findMany({
+    orderBy: {
+      createdAt: "desc",
+    },
+  });
+
+  return Response.json(products);
+}
+
+export async function POST(req: Request) {
+  const body = await req.json();
+
+  const product = await prisma.product.create({
+    data: {
+      name: body.name,
+      category: body.category,
+      description: body.description,
+    },
+  });
+
+  return Response.json(product);
+}
